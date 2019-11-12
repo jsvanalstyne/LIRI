8536fc2effac238cd856c4692023fad96200b6d4
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-11-11 20:01:09 -0500

    added do what it says functionality. Concert and movie need work.

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path:'.env'});
+require("dotenv").config({ path: '.env' });
 var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
@@ -6,6 +6,80 @@ var Spotify = require('node-spotify-api');
 var moment = require("moment");
 var inquirer = require("inquirer");
 var spotify = new Spotify(keys.spotify);
+
+function spotifyThis(userSong) {
+  spotify.search({ type: 'track', query: userSong }, function (err, data) {
+    console.log("working")
+    console.log(!data);
+    if (!data === false) {
+      console.log("Your search did not return any results. Might I interest you in: \n'The Sign' \nby 'Ace of Base' \nfrom 'The Sign' album. \nHere is a link: https://open.spotify.com/album/5UwIyIyFzkM7wKeGtRJPgB")
+    }
+
+    // Song name
+
+    for (var i = 0; i < data.tracks.items.length; i++) {
+      console.log("-------------------------------");
+      // song name
+      console.log(JSON.stringify("Song name: " + data.tracks.items[i].name));
+      // song link
+      console.log(JSON.stringify("Song link: " + data.tracks.items[i].external_urls.spotify));
+      // Artist name
+      console.log(JSON.stringify("Artist: " + data.tracks.items[i].artists[0].name));
+      // album name
+      console.log(JSON.stringify("Album Name: " + data.tracks.items[i].album.name));
+      console.log("-------------------------------");
+    }
+  });
+}
+
+function concertThis(userBand) {
+  console.log("got a concert")
+  console.log("insdie concertThis function "+userBand);
+  console.log(typeof userBand)
+  var concertApi = "https://rest.bandsintown.com/artists/" + userBand + "/events?app_id=codingbootcamp";
+  axios.get(concertApi).then(
+    function (response) {
+      if (response.data.length === 0) {
+        console.log("No events found.")
+      } else {
+        for (var i = 0; i < response.data.length; i++) {
+          console.log("-------------------------------")
+          console.log(JSON.stringify("Venue: " + response.data[i].venue.name));
+          console.log(JSON.stringify("City: " + response.data[i].venue.city));
+          console.log("Date: " + moment(response.data[i].datetime).format('L'));
+        }
+      }
+    }
+  );
+
+}
+function movieThis(movieTitle) {
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+  axios.get(queryUrl).then(
+    function (response) {
+
+
+      if ((response.data.Response) === "False") {
+        console.log("Movie not found");
+      } else {
+
+        console.log("Movie Title: " + response.data.Title);
+        console.log("Year Released: " + response.data.Year);
+        console.log("imdbRating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value));
+        console.log("Produced in which country: " + response.data.Country);
+        console.log("Movie Language: " + response.data.Language);
+        console.log("Movie Plot: " + response.data.Plot);
+        console.log("Actors in movie: " + response.data.Actors);
+
+      }
+    });
+
+}
+
+
+
+
 inquirer.prompt([
   {
     type: "checkbox",
@@ -19,87 +93,52 @@ inquirer.prompt([
     name: "response"
 
   }
-]).then(function liriSwitch(user) {
+]).then(function (user) {
   // console.log("line 23"+user)
   // console.log("line 24" + user.question);
   // console.log(user.response);
   switch (user.question.toString()) {
     case 'spotify-this-song':
-      // console.log("did you go in here?")
-      spotify.search({ type: 'track', query: user.response }, function (err, data) {
-        console.log("working")
-       console.log(!data);
-        if (!data===false) {
-          console.log("Your search did not return any results. Might I interest you in: \n'The Sign' \nby 'Ace of Base' \nfrom 'The Sign' album. \nHere is a link: https://open.spotify.com/album/5UwIyIyFzkM7wKeGtRJPgB")        }
-        
-        // Song name
-        
-      for(var i=0; i<data.tracks.items.length; i++){
-        console.log("-------------------------------");
-        // song name
-        console.log(JSON.stringify("Song name: "+data.tracks.items[i].name));
-        // song link
-        console.log(JSON.stringify("Song link: "+data.tracks.items[i].external_urls.spotify));
-        // Artist name
-        console.log(JSON.stringify("Artist: " +data.tracks.items[i].artists[0].name));
-        // album name
-        console.log(JSON.stringify("Album Name: "+data.tracks.items[i].album.name));
-        console.log("-------------------------------");
-      }
-      });
 
+      spotifyThis(user.response)
       break;
     case "concert-this":
-      console.log("got a concert")
-      var concertApi = "https://rest.bandsintown.com/artists/" + user.response + "/events?app_id=codingbootcamp";
-      axios.get(concertApi).then(
-        function (response) {
-          if(response.data.length===0){
-            console.log("No events found.")
-          }else{
-          for(var i=0; i<response.data.length; i++){
-            console.log("-------------------------------")
-          console.log(JSON.stringify("Venue: "+response.data[i].venue.name));
-          console.log(JSON.stringify("City: "+response.data[i].venue.city));
-          console.log("Date: " +moment(response.data[i].datetime).format('L'));
-        }}}
-        );
+
+
+      concertThis(user.response);
       break;
     case "movie-this":
-      var queryUrl = "http://www.omdbapi.com/?t=" + user.response + "&y=&plot=short&apikey=trilogy";
-      axios.get(queryUrl).then(
-        function (response) {
-          //   console.log("Release Year: " + response.data.Year);
-          // console.log(response);
-          console.log("Movie Title: " + response.data.Title);
-          console.log("Year Released: " + response.data.Year);
-          console.log("imdbRating: " + response.data.imdbRating);
-          console.log("Rotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value));
-          console.log("Produced in which country: " + response.data.Country);
-          console.log("Movie Language: " + response.data.Language);
-          console.log("Movie Plot: " + response.data.Plot);
-          console.log("Actors in movie: " + response.data.Actors);
 
-        });
+      movieThis(user.response);
       break;
     case "do-what-it-says":
-        fs.readFile("random.txt", "utf8", function(error, data) {
-          
+      fs.readFile("random.txt", "utf8", function (error, data) {
+
         console.log(data);
-        
+
         var doWhatitSays = data.split(",");
         console.log(doWhatitSays);
-        // liriSwitch(doWhatitSays[1])
-          // console.log(doWhatitSays[1]);
-          var switchItem = doWhatitSays[0];
-          var songMovieConcert= doWhatitSays[1];
-          console.log(switchItem);
-          liriSwitch(switchItem);
+        var switchItem = doWhatitSays[0];
+        var songMovieConcert = doWhatitSays[1];
+        console.log("before switch" +songMovieConcert);
+        switch (switchItem) {
+          case 'spotify-this-song':
 
-         
-          })
+            spotifyThis(songMovieConcert);
+            break;
+          case "concert-this":
+            console.log("inside switch "+ songMovieConcert);
+
+            concertThis(songMovieConcert);
+            break;
+          case "movie-this":
+
+            movieThis(songMovieConcert);
+            break;
+        }
+      })
       break;
     default:
-    console.log("Please select a viable option");
+      console.log("Please select a viable option");
   }
 })
